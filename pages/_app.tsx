4242bb8c20ab7634013eabbd8048d9251624977c
Anton Palermo <ANPBusinessmail@gmail.com>
2021-11-29 19:09:06 +0800

    init apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,20 @@ import { AppProps } from 'next/app'
 
 import '../styles/generic.css'
 
+import { ApolloProvider, InMemoryCache, ApolloClient } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'https://human-engine.herokuapp.com/',
+  cache: new InMemoryCache(),
+  credentials: 'include',
+})
+
 const App: React.FunctionComponent<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
+import { gql, useQuery } from '@apollo/client'
 
 const Home = () => {
-  return <h1>Sample</h1>
+  const GET_USERS = gql`
+    query Query {
+      users {
+        id
+        firstname
+        lastname
+        email
+        dateCreated
+        dateUpdated
+      }
+    }
+  `
+
+  const { data, loading, error } = useQuery(GET_USERS)
+
+  if (loading) return <h1>Loading...</h1>
+  if (error) return <h1>Error!</h1>
+
+  return (
+    <div>
+      <h1>Data</h1>
+      <h2>Queried Users</h2>
+      <hr />
+      {data.users.map((user: any, index: any) => (
+        <div key={index}>
+          <p>firstname: {user.firstname}</p>
+          <p>lastname: {user.lastname}</p>
+          <p>email: {user.email}</p>
+          <hr />
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default Home
